dee9bbd03414a8f19203cd12068bb1846da0030a
Marcel van Eijk <marcelve2112@gmail.com>
2025-12-29 14:36:10 +1300

    bash script for running dev environment

---
+++ build-scripts/dev.sh
@@ -0,0 +1,23 @@
+cd ..
+source ./.venv/bin/activate
+cd api
+
+NEW_DEPS=false
+
+while [ ! $# -eq 0 ]; do
+    case "$1" in 
+        --new-deps)
+            NEW_DEPS=true
+            ;;
+    esac
+    shift
+done
+
+if [ "$NEW_DEPS" = true ]; then
+    pip-compile requirements.in
+fi
+
+pip install -r requirements.txt
+cd ..
+cd api/app
+fastapi run main.py
