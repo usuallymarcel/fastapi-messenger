94041ffda639b943927a29ddc8568d11e04586a2
usuallymarcel <marcelve2112@gmail.com>
2026-01-01 17:25:58 +1300

    move websocket to route

--- api/app/main.py
+++ api/app/main.py
@@ -19,27 +19,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: list[WebSocket] = []
+# class ConnectionManager:
+#     def __init__(self):
+#         self.active_connections: list[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
+#     async def connect(self, websocket: WebSocket):
+#         await websocket.accept()
+#         self.active_connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
+#     def disconnect(self, websocket: WebSocket):
+#         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+#     async def send_personal_message(self, message: str, websocket: WebSocket):
+#         await websocket.send_text(message)
 
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+#     async def broadcast(self, message: str):
+#         for connection in self.active_connections:
+#             await connection.send_text(message)
 
 
-manager = ConnectionManager()
-messages = []
+# manager = ConnectionManager()
+# messages = []
 
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -58,23 +58,23 @@ async def config_js():
     return Response(content=f"""window.ENV = {{API_URL: "{settings.api_url}"}};""", media_type="application/javascript")
 
 
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    # client_join_msg = f"Client #{client_id} joined the chat"
-    # client_leave_msg = f"Client #{client_id} left the chat"
+# @app.websocket("/ws/{client_id}")
+# async def websocket_endpoint(websocket: WebSocket, client_id: int):
+#     # client_join_msg = f"Client #{client_id} joined the chat"
+#     # client_leave_msg = f"Client #{client_id} left the chat"
 
-    await manager.connect(websocket)
-    for msg in messages:
-        await manager.send_personal_message(msg, websocket)
-    # await manager.broadcast(client_join_msg)
-    # messages.append(client_join_msg)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            client_says_msg = f"Client #{client_id} says: {data}"
-            await manager.broadcast(client_says_msg)
-            messages.append(client_says_msg)
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        # await manager.broadcast(client_leave_msg)
-        # messages.append(client_leave_msg)
+#     await manager.connect(websocket)
+#     for msg in messages:
+#         await manager.send_personal_message(msg, websocket)
+#     # await manager.broadcast(client_join_msg)
+#     # messages.append(client_join_msg)
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             client_says_msg = f"Client #{client_id} says: {data}"
+#             await manager.broadcast(client_says_msg)
+#             messages.append(client_says_msg)
+#     except WebSocketDisconnect:
+#         manager.disconnect(websocket)
+#         # await manager.broadcast(client_leave_msg)
+#         # messages.append(client_leave_msg)
