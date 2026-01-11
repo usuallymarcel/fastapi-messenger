f06cdfc31c790bdcdaa51c87e9eb1e7161bbe3b9
Marcel van Eijk <marcelve2112@gmail.com>
2026-01-11 22:04:46 +1300

    crud operations for group stuff

---
+++ api/app/crud/group_messages.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from app.models.group_messages import GroupMessage
+
+def create_group_message(db: Session, group_id: int, sender_id: int, content: str) -> GroupMessage:
+    group_message = GroupMessage(
+        group_id= group_id,
+        sender_id=sender_id,
+        content=content,
+    )
+
+    db.add(group_message)
+    db.commit()
+
+    return group_message
+
+def get_messages_by_group(db: Session, group_id: int, take: int) -> list[GroupMessage]:
+    return db.query(GroupMessage
+                        ).filter(GroupMessage.group_id == group_id
+                        ).order_by(GroupMessage.created_at.desc()).limit(take).all()
+
---
+++ api/app/crud/groups.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+from app.models.groups import Group
+
+def create_group(db: Session, name: str, created_by: int, is_private: bool = False) -> Group:
+    group = Group(
+        name = name,
+        created_by=created_by,
+        is_private=is_private
+    )
+    db.add(group)
+    db.commit()
+
+    return group
+
+def get_group_by_id(db: Session, id: int) -> Group:
+    return db.query(Group).filter(Group.id == id).first()
+
+def update_group_name(db: Session, id: int, name: str) -> None:
+    db.query(Group).filter(Group.id == id).update(Group.name == name)
+    db.commit()
+
+def update_is_private(db: Session, id: int, is_private: bool) -> None:
+    db.query(Group).filter(Group.id == id).update(Group.is_private == is_private)
+    db.commit()
+
+def delete_group_by_id(db: Session, id: int) -> None:
+    db.query(Group).filter(Group.id == id).delete()
+    db.commit()
