cf37c3bbb0a2c6321a4182cf17ee4444e11bc193
usuallymarcel <marcelve2112@gmail.com>
2026-09-05 23:19:14 +1200

    move db into shared infra project

--- api/app/main.py
+++ api/app/main.py
@@ -20,7 +20,7 @@ app.include_router(groups.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "api.marcel.co.nz"],
+    allow_origins=["http://localhost:8000", "chat.marcel.co.nz"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
